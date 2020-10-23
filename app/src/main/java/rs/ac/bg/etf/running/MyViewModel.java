9dbc78eca2724cbbda5aa150fe6ff92351228447
Marko Micovic <marko.micovic.93@gmail.com>
2020-10-23 16:23:18 +0200

    Add init by instance state Bundle

--- app/src/main/java/rs/ac/bg/etf/running/MyViewModel.java
+++ app/src/main/java/rs/ac/bg/etf/running/MyViewModel.java
@@ -1,18 +1,35 @@
 package rs.ac.bg.etf.running;
 
+import android.os.Bundle;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
 
+    public static final String CALORIES_BURNED_KEY = "calories-burned";
+
+    private boolean caloriesBurnedValid = false;
     private final MutableLiveData<Integer> caloriesBurned = new MutableLiveData<>();
+
+    public void initByInstanceStateBundle(Bundle bundle) {
+        if (bundle != null) {
+            if (!caloriesBurnedValid) {
+                if (bundle.containsKey(CALORIES_BURNED_KEY)) {
+                    caloriesBurnedValid = true;
+                    caloriesBurned.setValue(bundle.getInt(CALORIES_BURNED_KEY));
+                }
+            }
+        }
+    }
 
     public LiveData<Integer> getCaloriesBurned() {
         return caloriesBurned;
     }
 
     public void setCaloriesBurned(int value) {
+        caloriesBurnedValid = true;
         caloriesBurned.setValue(value);
     }
 }
