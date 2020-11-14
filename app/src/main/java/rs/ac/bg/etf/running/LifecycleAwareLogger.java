3a9a19f7b8a5cc0b6d160eeb3fa6e317987c2014
Marko Micovic <marko.micovic.93@gmail.com>
2020-11-14 15:29:10 +0100

    Change LifecycleAwareLogger

--- app/src/main/java/rs/ac/bg/etf/running/LifecycleAwareLogger.java
+++ app/src/main/java/rs/ac/bg/etf/running/LifecycleAwareLogger.java
@@ -6,38 +6,44 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
-public class MyLifecycleAwareComponent implements LifecycleObserver {
+public class LifecycleAwareLogger implements LifecycleObserver {
 
-    private static final String LOG_TAG = "activity-lifecycle";
+    private final String logTag;
+    private final String lifecycleOwner;
+
+    public LifecycleAwareLogger(String logTag, String lifecycleOwner) {
+        this.logTag = logTag;
+        this.lifecycleOwner = lifecycleOwner;
+    }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_CREATE)
     public void myOnCreate() {
-        Log.d(LOG_TAG, "myOnCreate() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnCreate()");
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_START)
     public void myOnStart() {
-        Log.d(LOG_TAG, "myOnStart() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnStart()");
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_RESUME)
     public void myOnResume() {
-        Log.d(LOG_TAG, "myOnResume() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnResume()");
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_PAUSE)
     public void myOnPause() {
-        Log.d(LOG_TAG, "myOnPause() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnPause()");
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_STOP)
     public void myOnStop() {
-        Log.d(LOG_TAG, "myOnStop() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnStop()");
     }
 
     @OnLifecycleEvent(value = Lifecycle.Event.ON_DESTROY)
     public void myOnDestroy() {
-        Log.d(LOG_TAG, "myOnDestroy() called");
+        Log.d(logTag, "called " + lifecycleOwner +".myOnDestroy()");
     }
 
 }
