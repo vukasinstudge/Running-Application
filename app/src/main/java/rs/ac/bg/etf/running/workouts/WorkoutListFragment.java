f7681ea9fe1c6dd0a46ccd20fbda1b26de00de44
Marko Micovic <marko.micovic.93@gmail.com>
2020-11-29 22:09:31 +0100

    Add WorkoutListFragment

---
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutListFragment.java
@@ -0,0 +1,55 @@
+package rs.ac.bg.etf.running.workouts;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rs.ac.bg.etf.running.MainActivity;
+import rs.ac.bg.etf.running.databinding.FragmentWorkoutListBinding;
+import rs.ac.bg.etf.running.workouts.WorkoutViewModel;
+
+public class WorkoutListFragment extends Fragment {
+
+    private FragmentWorkoutListBinding binding;
+    private WorkoutViewModel workoutViewModel;
+    private NavController navController;
+    private MainActivity mainActivity;
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) requireActivity();
+        workoutViewModel = new ViewModelProvider(mainActivity).get(WorkoutViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWorkoutListBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+}
