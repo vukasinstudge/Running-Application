5582d45a638badd61eecdc2e7dc5e1fc4d7931d5
Marko Micovic <marko.micovic.93@gmail.com>
2020-11-29 21:44:36 +0100

    Cleanup RouteDetailsFragment

--- app/src/main/java/rs/ac/bg/etf/running/routes/RouteDetailsFragment.java
+++ app/src/main/java/rs/ac/bg/etf/running/routes/RouteDetailsFragment.java
@@ -2,8 +2,12 @@ package rs.ac.bg.etf.running.routes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,23 +20,36 @@ public class RouteDetailsFragment extends Fragment {
 
     private FragmentRouteDetailsBinding binding;
     private RouteViewModel routeViewModel;
+    private NavController navController;
+    private MainActivity mainActivity;
 
     public RouteDetailsFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) requireActivity();
+        routeViewModel = new ViewModelProvider(mainActivity).get(RouteViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentRouteDetailsBinding.inflate(inflater, container, false);
 
-        MainActivity parentActivity = (MainActivity) getParentFragment().getActivity();
-
-        routeViewModel = new ViewModelProvider(parentActivity).get(RouteViewModel.class);
-
         Route selectedRoute = routeViewModel.getRoutes().get(
                 RouteDetailsFragmentArgs.fromBundle(requireArguments()).getRouteIndex());
+
+        binding.toolbar.setTitle(selectedRoute.getLabel());
+        binding.toolbar.setNavigationOnClickListener(view -> {
+            navController.navigateUp();
+        });
 
         binding.routeImage.setImageDrawable(selectedRoute.getImage());
         binding.routeLabel.setText(selectedRoute.getLabel());
@@ -42,5 +59,11 @@ public class RouteDetailsFragment extends Fragment {
         binding.routeDescription.setText(selectedRoute.getDescription());
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 }
