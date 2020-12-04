ae9f4f12d87cd4c5671b28bbb00fd1da136221c3
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-05 00:27:35 +0100

    Add @HiltAndroidApp annotation

---
+++ app/src/main/java/rs/ac/bg/etf/running/RunApplication.java
@@ -0,0 +1,9 @@
+package rs.ac.bg.etf.running;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class RunApplication extends Application {
+}
