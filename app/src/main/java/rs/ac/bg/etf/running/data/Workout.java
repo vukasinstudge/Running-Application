638ce213bb4dc7095b630a196ef3a98e28a3f66c
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-04 18:14:37 +0100

    Make Entity fields accessible

--- app/src/main/java/rs/ac/bg/etf/running/data/Workout.java
+++ app/src/main/java/rs/ac/bg/etf/running/data/Workout.java
@@ -15,4 +15,52 @@ public class Workout {
     private String label;
     private double distance;
     private double duration;
+
+    public Workout(long id, Date date, String label, double distance, double duration) {
+        this.id = id;
+        this.date = date;
+        this.label = label;
+        this.distance = distance;
+        this.duration = duration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
