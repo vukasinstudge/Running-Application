812e7c9a809344ccdfacf19389dd1a9fe309f600
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-04 19:18:26 +0100

    Add WorkoutCreateFragment

---
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutCreateFragment.java
@@ -0,0 +1,58 @@
+package rs.ac.bg.etf.running.workouts;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rs.ac.bg.etf.running.MainActivity;
+import rs.ac.bg.etf.running.databinding.FragmentWorkoutCreateBinding;
+
+public class WorkoutCreateFragment extends Fragment {
+
+    private FragmentWorkoutCreateBinding binding;
+    private WorkoutViewModel workoutViewModel;
+    private NavController navController;
+    private MainActivity mainActivity;
+
+    public WorkoutCreateFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) requireActivity();
+        workoutViewModel = new ViewModelProvider(mainActivity).get(WorkoutViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWorkoutCreateBinding.inflate(inflater, container, false);
+
+        binding.toolbar.setNavigationOnClickListener(
+                view -> navController.navigateUp());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+
+}
