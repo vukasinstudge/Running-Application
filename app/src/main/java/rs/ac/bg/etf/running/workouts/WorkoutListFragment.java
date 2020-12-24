e7f69cb57bd1c49647a63462f08e2f0e4f23b7ff
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-24 13:24:51 +0100

    Replace OnClickListener with OnActionSelectedListener

--- app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutListFragment.java
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutListFragment.java
@@ -69,9 +69,15 @@ public class WorkoutListFragment extends Fragment {
 
         binding.floatingActionButton.inflate(R.menu.workout_list_fab_menu);
 
-        binding.floatingActionButton.setOnClickListener(view -> {
-            NavDirections action = WorkoutListFragmentDirections.createWorkout();
-            navController.navigate(action);
+        binding.floatingActionButton.setOnActionSelectedListener(actionItem -> {
+            switch (actionItem.getId()) {
+                case R.id.workout_fab_create:
+                    navController.navigate(WorkoutListFragmentDirections.createWorkout());
+                    return false;
+                case R.id.workout_fab_start:
+                    return false;
+            }
+            return true;
         });
 
         return binding.getRoot();
