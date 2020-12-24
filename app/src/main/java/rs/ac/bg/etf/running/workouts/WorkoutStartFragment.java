bf2a9675e5ecaf84a5129e2ab096788d0df9971f
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-24 13:48:40 +0100

    Add startWorkout() without SharedPreferences

--- app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutStartFragment.java
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutStartFragment.java
@@ -9,9 +9,15 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import dagger.hilt.android.AndroidEntryPoint;
 import rs.ac.bg.etf.running.MainActivity;
@@ -24,6 +30,8 @@ public class WorkoutStartFragment extends Fragment {
     private WorkoutViewModel workoutViewModel;
     private NavController navController;
     private MainActivity mainActivity;
+
+    private Timer timer;
 
     public WorkoutStartFragment() {
         // Required empty public constructor
@@ -45,6 +53,10 @@ public class WorkoutStartFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentWorkoutStartBinding.inflate(inflater, container, false);
 
+        timer = new Timer();
+
+        binding.start.setOnClickListener(view -> startWorkout(new Date().getTime()));
+
         return binding.getRoot();
     }
 
@@ -52,6 +64,41 @@ public class WorkoutStartFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        timer.cancel();
+    }
+
+    private void startWorkout(long startTimestamp) {
+        binding.start.setEnabled(false);
+        binding.finish.setEnabled(true);
+        binding.cancel.setEnabled(true);
+        binding.power.setEnabled(true);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                long elapsed = new Date().getTime() - startTimestamp;
+
+                int miliseconds = (int) ((elapsed % 1000) / 10);
+                int seconds = (int) ((elapsed / 1000) % 60);
+                int minutes = (int) ((elapsed / (1000 * 60)) % 60);
+                int hours = (int) ((elapsed / (1000 * 60 * 60)) % 24);
+
+                StringBuilder workoutDuration = new StringBuilder();
+                workoutDuration.append(String.format("%02d", hours)).append(":");
+                workoutDuration.append(String.format("%02d", minutes)).append(":");
+                workoutDuration.append(String.format("%02d", seconds)).append(".");
+                workoutDuration.append(String.format("%02d", miliseconds));
+
+                handler.post(() -> binding.workoutDuration.setText(workoutDuration));
+            }
+        }, 0, 10);
     }
 
 }
