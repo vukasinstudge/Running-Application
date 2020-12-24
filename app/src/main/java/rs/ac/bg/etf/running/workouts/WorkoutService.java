5ae27243df1291b6e2248eb5d87760abf22e93c5
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-24 16:18:06 +0100

    Add PendingIntent to Notification

--- app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.running.workouts;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
@@ -17,6 +18,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -126,10 +128,19 @@ public class WorkoutService extends Service {
     }
 
     private Notification getNotification() {
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent
+                .getActivity(this, 0, intent, 0);
+
         return new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
                 .setSmallIcon(R.drawable.baseline_directions_run_black_24)
                 .setContentTitle(getString(R.string.workout_notification_content_title))
                 .setContentText(getString(R.string.workout_notification_content_text))
+                .setContentIntent(pendingIntent)
+                .setColorized(true)
+                .setColor(ContextCompat.getColor(this, R.color.teal_200))
                 .build();
     }
 }
