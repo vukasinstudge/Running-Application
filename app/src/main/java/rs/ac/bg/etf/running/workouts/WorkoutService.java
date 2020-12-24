a6e4444e000dce631eccff13d6f8cbd75bc63969
Marko Micovic <marko.micovic.93@gmail.com>
2020-12-24 15:25:33 +0100

    Make service a foreground service

--- app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
+++ app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
@@ -1,7 +1,11 @@
 package rs.ac.bg.etf.running.workouts;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -10,6 +14,9 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +25,9 @@ import rs.ac.bg.etf.running.MainActivity;
 import rs.ac.bg.etf.running.R;
 
 public class WorkoutService extends Service {
+
+    private static final String NOTIFICATION_CHANNEL_ID = "workout-notification-channel";
+    private static final int NOTIFICATION_ID = 1;
 
     private final Timer timer = new Timer();
 
@@ -48,6 +58,10 @@ public class WorkoutService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(MainActivity.LOG_TAG, "WorkoutService.onStartCommand()");
+
+        createNotificationChannel();
+        startForeground(NOTIFICATION_ID, getNotification());
+
         if (!serviceStarted) {
             scheduleTimer();
         }
@@ -66,5 +80,21 @@ public class WorkoutService extends Service {
         Log.d(MainActivity.LOG_TAG, "WorkoutService.onDestroy()");
         super.onDestroy();
         timer.cancel();
+    }
+
+    private void createNotificationChannel() {
+        NotificationChannelCompat notificationChannel = new NotificationChannelCompat
+                .Builder(NOTIFICATION_CHANNEL_ID, NotificationManagerCompat.IMPORTANCE_DEFAULT)
+                .setName(getString(R.string.workout_notification_channel_name))
+                .build();
+        NotificationManagerCompat.from(this).createNotificationChannel(notificationChannel);
+    }
+
+    private Notification getNotification() {
+        return new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_directions_run_black_24)
+                .setContentTitle(getString(R.string.workout_notification_content_title))
+                .setContentText(getString(R.string.workout_notification_content_text))
+                .build();
     }
 }
